03c1866aa249d4d5e34e061d2fbb7010d67efb7a
Rafael Amaral de OLiveira <rafacienciacomputacao@gmail.com>
2024-05-11 07:16:51 -0300

    Organizing view.py into a Python package without breaking the code

---
+++ contact/views/__init__.py
@@ -0,0 +1,15 @@
+# flake8: noqa
+"""
+This script organizes and manages views related to contact management
+functionalities.
+
+**Purpose:**
+
+- To centralize and streamline the implementation of views for contact-related
+actions.
+- To promote code modularity and maintainability by separating views into a
+dedicated module.
+- To facilitate easy access to view functions within the current module or
+other parts of the application.
+"""
+from .contact_views import *
