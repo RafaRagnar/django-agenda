90b984cce16e0d33cdf96ef567cb9507deebe059
Rafael Amaral de OLiveira <rafacienciacomputacao@gmail.com>
2024-05-14 14:59:42 -0300

    login_required - ensuring that the user is logged in to access view

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 from contact.forms import RegisterForm, RegisterUpdateForm
 
 
@@ -18,6 +19,34 @@ def register(request):
     return render(
         request, 'contact/register.html', {'form': form}
     )
+
+
+@login_required(login_url='contact:login')
+def user_update(request):
+    form = RegisterUpdateForm(instance=request.user)
+
+    if request.method != 'POST':
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+
+    form = RegisterUpdateForm(data=request.POST, instance=request.user)
+
+    if not form.is_valid():
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+
+    form.save()
+    return redirect('contact:user_update')
 
 
 def login_view(request):
@@ -39,26 +68,7 @@ def login_view(request):
     )
 
 
-def user_update(request):
-    form = RegisterUpdateForm(instance=request.user)
-
-    if request.method != 'POST':
-        return render(
-            request, 'contact/user_update.html', {'form': form}
-        )
-
-    form = RegisterUpdateForm(data=request.POST, instance=request.user)
-
-    if not form.is_valid():
-        return render(
-            request, 'contact/user_update.html', {'form': form}
-        )
-
-    form.save()
-
-    return redirect('contact:user_update')
-
-
+@login_required(login_url='contact:login')
 def logout_view(request):
     auth.logout(request)
     return redirect('contact:login')
