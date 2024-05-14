9935ce128664bb63098294f1c59771a4faf5f63e
Rafael Amaral de OLiveira <rafacienciacomputacao@gmail.com>
2024-05-14 14:42:38 -0300

    Creating a form to update user data

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.forms import AuthenticationForm
-from contact.forms import RegisterForm
+from contact.forms import RegisterForm, RegisterUpdateForm
 
 
 def register(request):
@@ -37,6 +37,26 @@ def login_view(request):
     return render(
         request, 'contact/login.html', {'form': form}
     )
+
+
+def user_update(request):
+    form = RegisterUpdateForm(instance=request.user)
+
+    if request.method != 'POST':
+        return render(
+            request, 'contact/user_update.html', {'form': form}
+        )
+
+    form = RegisterUpdateForm(data=request.POST, instance=request.user)
+
+    if not form.is_valid():
+        return render(
+            request, 'contact/user_update.html', {'form': form}
+        )
+
+    form.save()
+
+    return redirect('contact:user_update')
 
 
 def logout_view(request):
